fbb3909dcee9ad96bd28bcb7b0c99ccc67f8f3ae
Jeremias Moraes <moraesjeremias@gmail.com>
2020-01-29 18:56:54 -0300

    Index.js init

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json()
+    return response.json({message: "Foi, será?"})
 });
 
 app.listen(3434);
