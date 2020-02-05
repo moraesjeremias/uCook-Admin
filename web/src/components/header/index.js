bc104673cf42cee105220c7c04c0da76d60ee99d
Jeremias Moraes <moraesjeremias@gmail.com>
2020-02-05 19:30:18 -0300

    Header Component Add

--- web/src/components/header/index.js
+++ web/src/components/header/index.js
@@ -1,17 +1,18 @@
 import React  from 'react';
-import 'styles.css';
+import './styles.css';
 import ucookLogo from '../../assets/Asset-2.png';
 
 
 const Header = () => {
     return <nav className="menu-nav">
-        <div>
-            <ucookLogo />
+        <div className="brand">
+            <img className="logo" src={ucookLogo} alt="uCook brand logo"/>
             <h2 className="title">uCook</h2>
+        </div>
+        <div className="entry">
+            <p>Cadastro</p>
         </div>
     </nav>
 }
-
-
 
 export default Header;
